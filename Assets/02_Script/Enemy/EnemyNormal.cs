b6f0d21db27cf5d1499b2f40993617696ffcdbe2
mashumairesh <dragon123764@gmail.com>
2026-05-25 02:48:32 +0900

    [Dev Part 1] - Enemy Health Bar Add

--- Assets/02_Script/Enemy/EnemyNormal.cs
+++ Assets/02_Script/Enemy/EnemyNormal.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains;
+using MoreMountains.Feedbacks;
 
 public class EnemyNormal : EnemyBase
 {
-
+    [SerializeField] MMFeedbacks mFeedbacks;
+    [SerializeField] EnemyHealthModule mHealthModule;
 
     private void OnEnable()
     {
         Init();
+        mFeedbacks = this.gameObject.GetComponentInChildren<MMFeedbacks>();
     }
 
     public override void Hit(float Damage)
     {
         base.Hit(Damage);
         HPcurrunt -= Damage;
+        mFeedbacks.PlayFeedbacks();
+        mHealthModule.Update_Health(Mathf.Clamp01(HPcurrunt / HPmax));
         if (HPcurrunt <= 0)
         {
             Dead();
